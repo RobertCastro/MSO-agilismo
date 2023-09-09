3509cc0fc70dc1b2b57f2bc928cd20a0ac0d815c
Robert Castro <oneroberth@gmail.com>
2023-09-09 00:34:43 -0500

    Caso 2: dar_personas test_base_de_datos_con_una_persona (Rojo)

--- tests/test_dar_personas.py
+++ tests/test_dar_personas.py
@@ -17,3 +17,9 @@ class TestDarPersonas(unittest.TestCase):
         dar_personas = entrenamiento_en_forma.dar_personas()
         
         self.assertEqual(dar_personas, [], "La base de datos no está vacía")
+    
+    def test_base_de_datos_con_una_persona(self):
+        entrenamiento_en_forma = EntrenamientoEnForma()
+        dar_personas = entrenamiento_en_forma.dar_personas()
+        self.assertEqual(len(dar_personas), 1, "La base de datos no contiene exactamente una persona")
+
