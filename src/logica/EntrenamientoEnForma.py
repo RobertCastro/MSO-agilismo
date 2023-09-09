20e5b088ee77509a7669401e588857756425a46f
karentarazonaj <k.tarazonaj@uniandes.edu.co>
2023-09-09 11:26:15 -0500

    Caso 1: Lista de personas vacia (rojo)

--- src/logica/EntrenamientoEnForma.py
+++ src/logica/EntrenamientoEnForma.py
@@ -1,15 +1,27 @@
 from src.logica.FachadaEnForma import FachadaEnForma
+from src.modelo.declarative_base import Session
+from src.modelo.Persona import Persona
 
 class EntrenamientoEnForma(FachadaEnForma):
 
-    def __init__(self):
-        #Este constructor contiene los datos falsos para probar la interfaz
-        self.personas = [{'nombre': 'Federico', 'apellido': 'Contreras', 'edad': 15, 'talla': 1.53, 'peso': 50, 'brazo': 15, 'pecho': 80, 'cintura': 70, 'pierna': 35, 'fecha_retiro': '', 'razon_retiro': ''},
-                         {'nombre': 'Angelica', 'apellido': 'Mora', 'edad': 42, 'talla': 1.90, 'peso': 75, 'brazo': 18, 'pecho': 95, 'cintura': 76, 'pierna': 40, 'fecha_retiro': '2023-03-30', 'razon_retiro': 'Incapacidad'},
-                         {'nombre': 'Julian', 'apellido': 'Salazar', 'edad': 30, 'talla': 1.69, 'peso': 59, 'brazo': 17, 'pecho': 69, 'cintura': 60, 'pierna': 28, 'fecha_retiro': '2023-01-18', 'razon_retiro': 'Cambio de instructor'},
-                         {'nombre': 'Bruno', 'apellido': 'Galan', 'edad': 26, 'talla': 1.53, 'peso': 60, 'brazo': 16, 'pecho': 72, 'cintura': 54, 'pierna': 20, 'fecha_retiro': '', 'razon_retiro': ''},
-                        ]
-
     def dar_personas(self):
-        return self.personas.copy()
 
+        session = Session()
+        personas = session.query(Persona).all()
+
+        personas_dict = [
+            {
+                'id': persona.id,
+                'nombre': persona.nombre,
+                'apellido': persona.apellido,
+            }
+            for persona in personas
+        ]
+
+ 
+
+        session.close()
+
+ 
+
+        return personas_dict
--- tests/test_dar_personas.py
+++ tests/test_dar_personas.py
@@ -1,5 +1,23 @@
 import unittest
 from src.logica.EntrenamientoEnForma import EntrenamientoEnForma
+from src.modelo.declarative_base import Session
+from src.modelo.Persona import Persona
 
 class TestDarPersonas(unittest.TestCase):
+
+    def setUp(self):
+
+        self.session = Session()
+        self.session.query(Persona).delete()  
+        self.session.commit()
+
+    def test_base_de_datos_vacia(self):
+
+        self.session.add(Persona(nombre="Test", apellido="User")) 
+        self.session.commit()
+
+        entrenamiento_en_forma = EntrenamientoEnForma()
+        dar_personas = entrenamiento_en_forma.dar_personas()
+
+        self.assertEqual(dar_personas, [], "La base de datos no está vacía")
 
