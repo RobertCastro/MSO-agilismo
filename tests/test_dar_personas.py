fcb662706aa8302b0fcda6cc2439e2c606228cbb
Robert Castro <oneroberth@gmail.com>
2023-09-09 00:00:44 -0500

    Caso 1: dar_personas base_de_datos_vacia (Rojo)

--- tests/test_dar_personas.py
+++ tests/test_dar_personas.py
@@ -3,6 +3,10 @@ from src.logica.EntrenamientoEnForma import EntrenamientoEnForma
 
 class TestDarPersonas(unittest.TestCase):
 
-    
+    def test_base_de_datos_vacia(self):
+        entrenamiento_en_forma = EntrenamientoEnForma()
+        dar_personas = entrenamiento_en_forma.dar_personas()
+        
+        self.assertEqual(dar_personas, [], "La base de datos no está vacía")
 
 
