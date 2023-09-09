218687de7db4a37ac0af9d249bc5bbecba800ed1
Robert Castro <oneroberth@gmail.com>
2023-09-09 00:24:01 -0500

    Caso 1: dar_personas base_de_datos_vacia (Verde)

--- tests/test_dar_personas.py
+++ tests/test_dar_personas.py
@@ -1,12 +1,19 @@
 import unittest
 from src.logica.EntrenamientoEnForma import EntrenamientoEnForma
+from src.modelo.declarative_base import Session
+from src.modelo.Persona import Persona
 
 class TestDarPersonas(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        session = Session()
+        session.query(Persona).delete() 
+        session.commit()
+        session.close()
 
     def test_base_de_datos_vacia(self):
         entrenamiento_en_forma = EntrenamientoEnForma()
         dar_personas = entrenamiento_en_forma.dar_personas()
         
         self.assertEqual(dar_personas, [], "La base de datos no está vacía")
-
-
