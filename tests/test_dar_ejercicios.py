f19babd30f1c6d55b755e87133fd9d72f99100f1
Robert Castro <oneroberth@gmail.com>
2023-09-09 21:26:56 -0500

    Caso 5: Listar ejercicios con al menos un ejercicio (rojo)

--- tests/test_dar_ejercicios.py
+++ tests/test_dar_ejercicios.py
@@ -16,3 +16,9 @@ class TestDarEjercicios(unittest.TestCase):
         entrenamiento_en_forma = EntrenamientoEnForma()
         dar_ejercicios = entrenamiento_en_forma.dar_ejercicios()
         self.assertEqual(dar_ejercicios, [], "La base de datos no está vacía")
+
+    def test_listar_ejercicios_con_al_menos_uno(self):
+
+        entrenamiento_en_forma = EntrenamientoEnForma()
+        dar_ejercicios = entrenamiento_en_forma.dar_ejercicios()
+        self.assertGreaterEqual(len(dar_ejercicios), 1, "La base de datos contiene al menos una persona")
