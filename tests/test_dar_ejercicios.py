07efe3d65a75b9e12b3f331f0aa40548bfdc1f01
Robert Castro <oneroberth@gmail.com>
2023-09-09 21:28:24 -0500

    Caso 5: Listar ejercicios con al menos un ejercicio (verde)

--- tests/test_dar_ejercicios.py
+++ tests/test_dar_ejercicios.py
@@ -19,6 +19,10 @@ class TestDarEjercicios(unittest.TestCase):
 
     def test_listar_ejercicios_con_al_menos_uno(self):
 
+        self.session.add(Ejercicio(nombre="Correr", descripcion="Correr por 20 minutos", enlace="https://youtube.com", calorias=200))
+        self.session.add(Ejercicio(nombre="Abdominales", descripcion="Abdominales por 20 minutos", enlace="https://youtube.com", calorias=250)) 
+        self.session.commit()
+
         entrenamiento_en_forma = EntrenamientoEnForma()
         dar_ejercicios = entrenamiento_en_forma.dar_ejercicios()
         self.assertGreaterEqual(len(dar_ejercicios), 1, "La base de datos contiene al menos una persona")
